3a164890e7a636d71a96c62569118dcf3fbf5697
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2025-02-27 21:50:41 -0500

    feat: :sparkles: add exceptions and global handler

---
+++ src/main/java/com/example/library/exception/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,75 @@
+package com.example.library.exception.handlers;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.library.dto.ErrorResponseDto;
+import com.example.library.dto.ValidationErrorDto;
+import com.example.library.exception.NotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @Autowired
+  private MessageSource messageSource;
+
+  @ExceptionHandler(NotFoundException.class)
+  public ResponseEntity<ErrorResponseDto> handleNotFoundException(NotFoundException exception,
+      HttpServletRequest request) {
+
+    ErrorResponseDto errorResponseDto = ErrorResponseDto.builder()
+        .timestamp(new Date().getTime())
+        .status(HttpStatus.NOT_FOUND.value())
+        .title("Resource not found")
+        .detail(exception.getMessage())
+        .developerMessage(exception.getClass().getName())
+        .path(request.getRequestURI())
+        .build();
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponseDto);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValidException(
+      MethodArgumentNotValidException exception,
+      HttpServletRequest request) {
+
+    ErrorResponseDto errorResponseDto = ErrorResponseDto.builder()
+        .timestamp(new Date().getTime())
+        .status(HttpStatus.BAD_REQUEST.value())
+        .title("Validation failed")
+        .detail(exception.getMessage())
+        .developerMessage(exception.getClass().getName())
+        .path(request.getRequestURI())
+        .build();
+
+    List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+    for (FieldError fieldError : fieldErrors) {
+      List<ValidationErrorDto> validationErrorsDto = errorResponseDto.getErrors().get(fieldError.getField());
+      if (validationErrorsDto == null) {
+        validationErrorsDto = new ArrayList<ValidationErrorDto>();
+        errorResponseDto.getErrors().put(fieldError.getField(), validationErrorsDto);
+      }
+
+      ValidationErrorDto validationErrorDto = new ValidationErrorDto();
+      validationErrorDto.setCode(fieldError.getCode());
+      validationErrorDto.setMessage(messageSource.getMessage(fieldError, request.getLocale()));
+      validationErrorsDto.add(validationErrorDto);
+    }
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponseDto);
+  }
+
+}
