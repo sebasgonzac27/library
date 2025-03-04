b8dd143d57d9345055798787eb324d9572adbfca
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2025-03-04 00:22:47 -0500

    feat: :sparkles: set final to inject service

--- src/main/java/com/example/library/controller/AuthController.java
+++ src/main/java/com/example/library/controller/AuthController.java
@@ -20,7 +20,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 
-  private AuthService authService;
+  private final AuthService authService;
 
   @PostMapping("/sign-in")
   private ResponseEntity<TokenResponseDto> signIn(@RequestBody SignInRequestDto signInRequestDto) {
